6567817520ee9503d67e7b0a8f26c3f85f979011
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-29 14:46:03 +0600

    added cherry

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-var studentFirstName = "Bakyt";
-var studentLastName = "Utanov";
-var studentAge = 20;
-var studentOccupied = false;
+// var studentFirstName = "Bakyt";
+// var studentLastName = "Utanov";
+// var studentAge = 20;
+// var studentOccupied = false;
 
-var student = {
-  firstName: "Bakyt",
-  lastName: "Utanov",
-  age: 20,
-  occupied: false,
-  sayHello: function () {
-    console.log("Hello my name is " + this.firstName + " and I'm " + this.age + " years old!");
-  }
-};
+// var student = {
+//   firstName: "Bakyt",
+//   lastName: "Utanov",
+//   age: 20,
+//   occupied: false,
+//   sayHello: function () {
+//     console.log("Hello my name is " + this.firstName + " and I'm " + this.age + " years old!");
+//   }
+// };
 
-// output
-console.log(student.firstName + " " + student.lastName);
+// // output
+// console.log(student.firstName + " " + student.lastName);
 
-var dog = {
-  name: "Kumaiyk",
-  age: 2,
-};
+// var dog = {
+//   name: "Kumaiyk",
+//   age: 2,
+// };
 
-//update
-dog.age = 3;
+// //update
+// dog.age = 3;
 
-//add
-dog.color = "gray";
-// console.log(dog.color);
+// //add
+// dog.color = "gray";
+// // console.log(dog.color);
 
-//delete
-delete dog.color;
+// //delete
+// delete dog.color;
 
-//Methods
-dog.bark = function () {
-  console.log("Woof, woof, wof!");
-}
+// //Methods
+// dog.bark = function () {
+//   console.log("Woof, woof, wof!");
+// }
+// // dog.bark();
 // dog.bark();
-dog.bark();
 
-student.firstName = "Aijan";
-student.age = 19;
-student.sayHello();
+// student.firstName = "Aijan";
+// student.age = 19;
+// student.sayHello();
 
 
 //ARRAYs
@@ -48,6 +48,16 @@ student.sayHello();
 var shoppingList1 = "Apple";
 var shoppingList2 = "Pear"
 
-var shoppingList = ["Apple", "Pear", "Pineapple", "Cherry", "Tomato", "Potato" 100, true];
+var shoppingList = ["Apple", "Pear", "Pineapple", "Cherry", "Tomato", "Potato", 100, true];
 
 console.log(shoppingList [0]);
+console.log(shoppingList [1]);
+console.log(shoppingList [6]);
+
+// update
+shoppingList [2] = "Tomato";
+//console.log(shoppingList [2]);
+
+// add to the end
+shoppingList.push("Cherry");
+// console.log(shoppingList);

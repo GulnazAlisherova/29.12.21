219579b456a5eb29f3bb02a19c03228da028daa9
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2021-12-29 14:12:38 +0600

    added console

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ var studentFirstName = "Bakyt";
 var studentLastName = "Utanov";
 var studentAge = 20;
 var studentOccupied = false;
+
+var student = {
+  firstName: "Bakyt";
+  lastName: "Utanov";
+  age: 20,
+  occupied: false,
+};
+
+// output
+console.log(studentFirstName);
